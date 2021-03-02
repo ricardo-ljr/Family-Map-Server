66a252580eda6dba9284e5448f1f93df317bf95c
Ricardo Leite <ricardo.leitejr95@gmail.com>
2021-03-01 19:00:42 -0700

    Started DAO and tests

---
+++ test/DAO/PersonDaoTest.java
@@ -0,0 +1,24 @@
+package DAO;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonDaoTest {
+
+    @Test
+    void testAddPerson() {
+
+        //assertEquals("fred", obj.getValue());
+        //assertFalse
+        //assertThrows
+    }
+
+    @Test
+    void testFindPerson() {
+    }
+
+    @Test
+    void testClearPerson() {
+    }
+}
---
+++ test/DAO/UserDaoTest.java
@@ -0,0 +1,20 @@
+package DAO;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+
+    @Test
+    void registerUser() {
+    }
+
+    @Test
+    void findUser() {
+    }
+
+    @Test
+    void clearUser() {
+    }
+}
