6684ddee87a213a7163fb48bd4d3e754e3b23219
Ricardo Leite <ricardo.leitejr95@gmail.com>
2021-03-10 16:47:09 -0700

    Clear Service and HTTP request works!

--- src/main/java/Handler/DefaultHandler.java
+++ src/main/java/Handler/DefaultHandler.java
@@ -1,4 +1,49 @@
 package Handler;
 
-public class DefaultHandler {
+import Result.MessageResult;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.*;
+
+public class DefaultHandler implements HttpHandler {
+
+    /**
+     * Handles the request and returns response
+     *
+     * @param exchange Contains the request
+     */
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {}
+
+    String readString(InputStream reqBody) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        InputStreamReader sr = new InputStreamReader(reqBody);
+        char[] buf = new char[1024];
+        int len;
+        while ((len = sr.read(buf)) > 0) {
+            sb.append(buf, 0, len);
+        }
+        return sb.toString();
+    }
+
+    protected void generate(MessageResult result, OutputStream output) throws IOException {
+        Gson gson;
+        if(result.getMessage() != null) {
+            gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+        }
+        else {
+            gson = new GsonBuilder().setPrettyPrinting().create();
+        }
+        String jsonString = gson.toJson(result);
+        OutputStreamWriter outputWriter = new OutputStreamWriter(output);
+        BufferedWriter bufferedWriter = new BufferedWriter(outputWriter);
+        bufferedWriter.write(jsonString);
+        bufferedWriter.flush();
+
+        System.out.println(jsonString);
+    }
+
 }
---
+++ src/test/java/Services/ClearServiceTest.java
@@ -0,0 +1,17 @@
+package Services;
+
+import Result.MessageResult;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClearServiceTest {
+
+    @Test
+    void clearResult() {
+        ClearService clearService = new ClearService();
+        MessageResult myResult = clearService.clearResult();
+        MessageResult expected = new MessageResult("Cleared database successfully.");
+        assertEquals(myResult.getMessage(), expected.getMessage());
+    }
+}
